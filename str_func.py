9a76ff3f633986833fcb435391b77007fbe8e41e
AleksandrZaec <zaecam95@mail.ru>
2023-07-18 19:22:49 +0300

    Добавил докстринг к первой функции, добавил функцию которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции.

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def convert_to_uppercase(string):
+    """функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами."""
     return string.upper()
+
+def capitalize_first_letters(string):
+    """функцию, которая делает заглавными первые буквы каждого слова в строке,
+    поступившей на вход функции."""
+    return string.title()
